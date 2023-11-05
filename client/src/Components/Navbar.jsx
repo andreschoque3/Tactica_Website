8d7850dd6158c89db825112c28bce041498736fb
Andres Choque <andreschoque71@gmail.com>
2023-11-04 23:11:04 -0400

    major changes

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='nav-logo'>
+          <p>tactica logo</p>
+      </div>
+
+      <div>
+        <ul className='nav-menu'>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
