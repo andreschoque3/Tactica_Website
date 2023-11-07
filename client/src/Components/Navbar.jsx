d93906bf941a2cb7049cc83eb358b0d321920c10
Andres Choque <andreschoque71@gmail.com>
2023-11-06 21:44:18 -0500

    navbar changes

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,25 +1,39 @@
 import React from 'react'
 import './Navbar.css'
+import logo from './Assets/logo_tactica_global.jpeg'
+//import { Link } from 'react-router-dom'
 
 
 function Navbar() {
   return (
     <div className='navbar'>
-      <div className='nav-logo'>
-          <p>tactica logo</p>
+      <div className='nav-top'>
+        <nav className='first'>
+          <img src={logo} alt="logo" className='logo' />
+          <ul className='icon-menu'>
+            <li><i className='fas fa-user'></i></li>
+            <li><i className='fas fa-globe'></i></li>
+          </ul>
+        </nav>
       </div>
 
       <div>
-        <ul className='nav-menu'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Blog</li>
-          <li>Serve</li>
-          <li>Give</li>
-          <li>Divisions</li>
-          <li>Contact</li>
-          <li>Shop</li>
-        </ul>
+        <hr />
+      </div>
+
+
+      <div className='nav-bottom'>
+        <nav className='second'>
+          <ul className='nav-menu'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Blog</li>
+            <li>Serve</li>
+            <li>Give</li>
+            <li>Global</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
       </div>
     </div>
   )
