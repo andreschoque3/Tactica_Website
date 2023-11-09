b9946ab1f6a50dfa3ff16b855c3a308c16eb0b9b
Andres Choque <andreschoque71@gmail.com>
2023-11-08 22:07:00 -0500

    improved changes added footer

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from 'react'
+import './Styles/Navbar.css'
 import logo from './Assets/logo_tactica_global.jpeg'
 //import { Link } from 'react-router-dom'
 
 
 function Navbar() {
+
   return (
     <div className='navbar'>
       <div className='nav-top'>
         <nav className='first'>
-          <img src={logo} alt="logo" className='logo' />
+          <img src={logo} alt="logo" className='logo-top' />
           <ul className='icon-menu'>
             <li><i className='fas fa-user'></i></li>
             <li><i className='fas fa-globe'></i></li>
+            <li><i className='fas fa-circle-xmark'></i></li>
           </ul>
+          <i className='fas fa-bars'></i>
         </nav>
       </div>
 
       <div>
-        <hr />
+        <hr className='navbar-line' />
       </div>
 
 
