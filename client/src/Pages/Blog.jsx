7ba1ab7a23aec033b6c6ffe912c0000537138c51
Andres Choque <andreschoque71@gmail.com>
2023-11-20 15:02:23 -0500

    setting up blog page

--- client/src/Pages/Blog.jsx
+++ client/src/Pages/Blog.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import './Styles/Blog.css'
+import Navbar from '../Components/Navbar/Navbar'
+import Footer from '../Components/Footer/Footer'
 
 const Blog = () => {
   return (
-  <div>
-    Blog page
+  <div className='blog=pg'>
+
+    <Navbar/>
+
+    <section className='header-section-blg'>
+
+    </section>
+
+    <section className='newsletter-sect'>
+
+    </section>
+
+    <section className='blog-posts'>
+
+    </section>
+
+    <Footer/>
+    
   </div>
   )
 }
